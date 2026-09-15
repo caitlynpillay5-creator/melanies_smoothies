1d96b6c67eb73306c8f780a0ac6b1d13f7cda7a7
caitlynpillay5-creator <caitlynpillay5@gmail.com>
2026-09-15 11:04:27 +0200

    Update streamlit_app.py
    for each fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,14 +69,19 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + " "
 
-
     my_insert_stmt = """
         INSERT INTO smoothies.public.orders
         (ingredients, name_on_order)
         VALUES ('""" + ingredients_string + """',
                 '""" + name_on_order + """')
     """
+    st.subheader("🍉 SmoothieFroot Information")
 
+    smoothiefroot_response = requests.get(
+    "https://my.smoothiefroot.com/api/fruit/watermelon"
+)
+
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     st.write(my_insert_stmt)
 
