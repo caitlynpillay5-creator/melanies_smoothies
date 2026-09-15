8c4ae6b2600c09f47863986d59828ef8ef86870b
caitlynpillay5-creator <caitlynpillay5@gmail.com>
2026-09-15 13:04:36 +0200

    Update streamlit_app.py
    insert

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+python
 # Import Python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
@@ -44,7 +45,11 @@ my_dataframe = (
     session
     .table("smoothies.public.fruit_options")
     .select(col("FRUIT_NAME"))
+    .collect()
 )
+
+# Convert Snowflake results into a Python list
+fruit_list = [row["FRUIT_NAME"] for row in my_dataframe]
 
 
 # ---------------------------------------------------------
@@ -53,7 +58,7 @@ my_dataframe = (
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:",
-    my_dataframe,
+    fruit_list,
     max_selections=5
 )
 
@@ -67,39 +72,4 @@ if ingredients_list:
     ingredients_string = ""
 
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + " "
-
-
-    my_insert_stmt = """
-        INSERT INTO smoothies.public.orders
-        (ingredients, name_on_order)
-        VALUES ('""" + ingredients_string + """',
-                '""" + name_on_order + """')
-    """
-
-
-    st.write(my_insert_stmt)
-
-
-    time_to_insert = st.button("Submit Order")
-
-
-    if time_to_insert:
-
-        session.sql(my_insert_stmt).collect()
-
-        st.success(
-            "Your Smoothie is ordered! 🥤",
-            icon="✅"
-        )
-
-
-# ---------------------------------------------------------
-# SMOOTHIEFROOT API
-# ---------------------------------------------------------
-
-st.subheader("🍉 SmoothieFroot Information")
-
-smoothiefroot_response = requests.get(
-    "https://my.smoothiefroot.com/api/fruit/watermelon"
-)
+        ingredients
