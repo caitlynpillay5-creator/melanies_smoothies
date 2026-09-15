4907478abb5bf10d0c3b9a91903f80bca9916b71
caitlynpillay5-creator <caitlynpillay5@gmail.com>
2026-09-15 11:15:49 +0200

    Update streamlit_app.py
    yeah neh

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,135 @@ import requests
 
 st.title("🥤 Customise your Smoothie! 🥤")
 
+st.write("Choose the fruits you want in your custom Smoothie!")
+
+
+# ---------------------------------------------------------
+# NAME OF SMOOTHIE
+# ---------------------------------------------------------
+
+name_on_order = st.text_input("Name of Smoothie")
+
+st.write(
+    "The name of your smoothie will be:",
+    name_on_order
+)
+
+
+# ---------------------------------------------------------
+# CONNECT TO SNOWFLAKE
+# ---------------------------------------------------------
+
+cnx = st.connection("snowflake")
+session = cnx.session()
+
+
+# ---------------------------------------------------------
+# GET FRUIT OPTIONS FROM SNOWFLAKE
+# ---------------------------------------------------------
+
+my_dataframe = (
+    session
+    .table("smoothies.public.fruit_options")
+    .select(col("FRUIT_NAME"))
+)
+
+
+# ---------------------------------------------------------
+# ALLOW USER TO SELECT INGREDIENTS
+# ---------------------------------------------------------
+
+ingredients_list = st.multiselect(
+    "Choose up to 5 ingredients:",
+    my_dataframe,
+    max_selections=5
+)
+
+
+# ---------------------------------------------------------
+# PROCESS SELECTED INGREDIENTS
+# ---------------------------------------------------------
+
+if ingredients_list:
+
+    ingredients_string = ""
+
+    st.subheader("🍎 Nutrition Information")
+
+
+    # -----------------------------------------------------
+    # GET NUTRITION INFORMATION FOR EACH SELECTED FRUIT
+    # -----------------------------------------------------
+
+    for fruit_chosen in ingredients_list:
+
+        ingredients_string += fruit_chosen + " "
+
+        st.write("### " + fruit_chosen)
+
+        # Convert fruit name into API-friendly format
+        search_fruit = fruit_chosen.lower().replace(" ", "")
+
+        smoothiefroot_response = requests.get(
+            "https://my.smoothiefroot.com/api/fruit/" + search_fruit
+        )
+
+        # Check that API request worked
+        if smoothiefroot_response.status_code == 200:
+
+            st.dataframe(
+                data=smoothiefroot_response.json(),
+                use_container_width=True
+            )
+
+        else:
+
+            st.warning(
+                "Nutrition information could not be found for "
+                + fruit_chosen
+            )
+
+
+    # -----------------------------------------------------
+    # CREATE SQL INSERT STATEMENT
+    # -----------------------------------------------------
+
+    my_insert_stmt = """
+        INSERT INTO smoothies.public.orders
+        (ingredients, name_on_order)
+        VALUES ('""" + ingredients_string + """',
+                '""" + name_on_order + """')
+    """
+
+
+    # -----------------------------------------------------
+    # SUBMIT ORDER
+    # -----------------------------------------------------
+
+    time_to_insert = st.button("Submit Order")
+
+    if time_to_insert:
+
+        session.sql(my_insert_stmt).collect()
+
+        st.success(
+            "Your Smoothie is ordered! 🥤",
+            icon="✅"
+        )
+
+On Tue, Sep 15, 2026 at 11:10 AM Caitlyn Pillay <cpillay@expeditus.co.za> wrote:
+# Import Python packages
+import streamlit as st
+from snowflake.snowpark.functions import col
+import requests
+
+
+# ---------------------------------------------------------
+# APP TITLE
+# ---------------------------------------------------------
+
+st.title("🥤 Customise your Smoothie! 🥤")
+
 st.write(
     """Choose the fruits you want in your custom Smoothie!"""
 )
@@ -68,12 +197,12 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + " "
-        
+       
     st.subheader("🍉 SmoothieFroot Information")
 
     smoothiefroot_response = requests.get(
-    "https://my.smoothiefroot.com/api/fruit/watermelon", 
-        
+    "https://my.smoothiefroot.com/api/fruit/watermelon",
+       
     my_insert_stmt = """
         INSERT INTO smoothies.public.orders
         (ingredients, name_on_order)
